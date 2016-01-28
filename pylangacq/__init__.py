e0549e8452775577185a84cdd0d0b2c94f80707a
JacksonLLee <jsllee.phon@gmail.com>
2016-01-28 00:34:35 -0600

    define "read_chat()" for convenience

--- pylangacq/__init__.py
+++ pylangacq/__init__.py
@@ -1,5 +1,21 @@
 import os
+from pylangacq.chat import Reader
 
+VERSION_ERROR = 'unknown version; VERSION file not found'
 version_filename = os.path.join(os.path.dirname(__file__), 'VERSION')
-with open(version_filename) as f:
-    __version__ = f.read().strip()
+
+try:
+    with open(version_filename) as f:
+        __version__ = f.read().strip()
+except FileNotFoundError:
+    __version__ = VERSION_ERROR
+
+
+def read_chat(*filenames):
+    """
+    Create a ``Reader`` object based on *filenames.
+
+    :param filenames: one or multiple filenames (absolute-path or relative to
+        the current directory; with or without glob matching patterns)
+    """
+    return Reader(*filenames)

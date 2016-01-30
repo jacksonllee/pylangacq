30527bf87b7c0f098f4dd821665751fc8bc7f645
JacksonLLee <jsllee.phon@gmail.com>
2016-01-30 13:35:29 -0600

    reorganize static methods for SingleReader

--- pylangacq/chat.py
+++ pylangacq/chat.py
@@ -899,7 +899,7 @@ class SingleReader:
 
         for old_index in range(len(result_with_collapses)):
             tier_dict = result_with_collapses[old_index]
-            participant_code = get_participant_code(tier_dict.keys())
+            participant_code = self._get_participant_code(tier_dict.keys())
             utterance = tier_dict[participant_code]
 
             try:
@@ -1176,7 +1176,7 @@ class SingleReader:
                 if tier_marker in participants:
                     line = tiermarker_to_line[tier_marker]
                     if clean:
-                        output.append((tier_marker, clean_utterance(line)))
+                        output.append((tier_marker, self._clean_utterance(line)))
                     else:
                         output.append((tier_marker, line))
                     break
@@ -1376,10 +1376,12 @@ class SingleReader:
 
         for i in range(self.number_of_utterances()):
             tiermarker_to_line = self._index_to_tiers[i]
-            participant_code = get_participant_code(tiermarker_to_line.keys())
+            participant_code = self._get_participant_code(
+                tiermarker_to_line.keys())
 
             # get the plain words from utterance tier
-            utterance = clean_utterance(tiermarker_to_line[participant_code])
+            utterance = self._clean_utterance(
+                tiermarker_to_line[participant_code])
             words = utterance.split()
 
             # %mor tier
@@ -1704,214 +1706,219 @@ class SingleReader:
 
             return result_list
 
+    @staticmethod
+    def _clean_utterance(utterance):
+        """
+        Filter away the CHAT-style annotations in *utterance*.
 
-def clean_utterance(utterance):
-    """
-    Filter away the CHAT-style annotations in *utterance*.
+        :param utterance: The utterance as a str
 
-    :param utterance: The utterance as a str
+        :return: The utterance without CHAT annotations
 
-    :return: The utterance without CHAT annotations
+        :rtype: str
+        """
+        # Function tested with the following CHILDES datasets:
+        # 1) Brown (Adam, Eve, and Sarah) in Eng-NA-MOR
+        # 2) Valian in Eng-NA-MOR
+        # 3) YipMatthews in Biling
+        # 4) LeeWongLeung in EastAsian/Cantonese
 
-    :rtype: str
-    """
-    # Function tested with the following CHILDES datasets:
-    # 1) Brown (Adam, Eve, and Sarah) in Eng-NA-MOR
-    # 2) Valian in Eng-NA-MOR
-    # 3) YipMatthews in Biling
-    # 4) LeeWongLeung in EastAsian/Cantonese
+        # *** At the end of each step, apply remove_extra_spaces(). ***
 
-    # *** At the end of each step, apply remove_extra_spaces(). ***
+        # Step 1: Remove unwanted scope elements (only the very certain cases)
+        # [= whatever] for explanations
+        # [x how_many_times] for collapses
+        # [+ whatever] for actions etc
+        # [* whatever] for error coding
+        # [=? whatever] for uncertain transcriptions
+        # [=! whatever] for actions etc
+        # [% whatever] for random noises?
+        # [- language_name] for using a non-dominant language
+        # [^ whatever] for complex local events
+        # whatever for audio/video time stamps? the  character is 0x15
+        # [<] and [>] for overlapping, including [<1], [>2] etc with numbers
+        # (2.), (3.5) etc for pauses
 
-    # Step 1: Remove unwanted scope elements (only the very certain cases), e.g.
-    # [= whatever] for explanations
-    # [x how_many_times] for collapses
-    # [+ whatever] for actions etc
-    # [* whatever] for error coding
-    # [=? whatever] for uncertain transcriptions
-    # [=! whatever] for actions etc
-    # [% whatever] for random noises?
-    # [- language_name] for using a non-dominant language
-    # [^ whatever] for complex local events
-    # whatever for audio/video time stamps? the  character is 0x15
-    # [<] and [>] for overlapping, including [<1], [>2] etc with numbers
-    # (2.), (3.5) etc for pauses
+        # [?] for best guess
+        # [!] for stressing
 
-    # [?] for best guess
-    # [!] for stressing
+        # "[*] [/" replaced by "[/"
+        # "] [*]" replaced by "]"
 
-    # "[*] [/" replaced by "[/"
-    # "] [*]" replaced by "]"
+        utterance = re.sub('\[= [^\[]+?\]', '', utterance)
+        utterance = re.sub('\[x \d+?\]', '', utterance)
+        utterance = re.sub('\[\+ [^\[]+?\]', '', utterance)
+        utterance = re.sub('\[\* [^\[]+?\]', '', utterance)
+        utterance = re.sub('\[=\? [^\[]+?\]', '', utterance)
+        utterance = re.sub('\[=! [^\[]+?\]', '', utterance)
+        utterance = re.sub('\[% [^\[]+?\]', '', utterance)
+        utterance = re.sub('\[- [^\[]+?\]', '', utterance)
+        utterance = re.sub('\[\^ [^\[]+?\]', '', utterance)
+        utterance = re.sub('[^]+?', '', utterance)
+        utterance = re.sub('\[<\d?\]', '', utterance)
+        utterance = re.sub('\[>\d?\]', '', utterance)
+        utterance = re.sub('\(\d+?\.?\d*?\)', '', utterance)
 
-    utterance = re.sub('\[= [^\[]+?\]', '', utterance)
-    utterance = re.sub('\[x \d+?\]', '', utterance)
-    utterance = re.sub('\[\+ [^\[]+?\]', '', utterance)
-    utterance = re.sub('\[\* [^\[]+?\]', '', utterance)
-    utterance = re.sub('\[=\? [^\[]+?\]', '', utterance)
-    utterance = re.sub('\[=! [^\[]+?\]', '', utterance)
-    utterance = re.sub('\[% [^\[]+?\]', '', utterance)
-    utterance = re.sub('\[- [^\[]+?\]', '', utterance)
-    utterance = re.sub('\[\^ [^\[]+?\]', '', utterance)
-    utterance = re.sub('[^]+?', '', utterance)
-    utterance = re.sub('\[<\d?\]', '', utterance)
-    utterance = re.sub('\[>\d?\]', '', utterance)
-    utterance = re.sub('\(\d+?\.?\d*?\)', '', utterance)
+        utterance = utterance.replace('[?]', '')
+        utterance = utterance.replace('[!]', '')
 
-    utterance = utterance.replace('[?]', '')
-    utterance = utterance.replace('[!]', '')
+        utterance = utterance.replace('[*] [/', '[/')
+        utterance = utterance.replace('] [*]', ']')
 
-    utterance = utterance.replace('[*] [/', '[/')
-    utterance = utterance.replace('] [*]', ']')
+        utterance = remove_extra_spaces(utterance)
+        # print('step 1:', utterance)
 
-    utterance = remove_extra_spaces(utterance)
-    # print('step 1:', utterance)
+        # Step 2: Pad elements with spaces to avoid human transcription errors
+        # If utterance has these delimiters: [ ]
+        # then pad them with extra spaces to avoid errors in transcriptions
+        # like "movement[?]" (--> "movement [?]")
+        #
+        # If utterance has:
+        #     < > (left and right angle brackets), excluding "+<" (lazy overlap)
+        #     “ (beginning quote)
+        #     ” (ending quote)
+        #     , (comma)
+        #     ? (question mark)
+        #     . (period) <-- commented out at the moment
+        # then pad them with extra spaces.
 
-    # Step 2: Pad elements with spaces to avoid human transcription errors etc
-    # If utterance has these delimiters: [ ]
-    # then pad them with extra spaces to avoid errors in transcriptions
-    # like "movement[?]" (--> "movement [?]")
-    #
-    # If utterance has:
-    #     < > (left and right angle brackets), excluding "+<" (lazy overlap)
-    #     “ (beginning quote)
-    #     ” (ending quote)
-    #     , (comma)
-    #     ? (question mark)
-    #     . (period) <-- commented out at the moment
-    # then pad them with extra spaces.
+        utterance = utterance.replace('<', ' <')
+        utterance = utterance.replace('+ <', '+<')
+        utterance = utterance.replace('>', '> ')
+        utterance = utterance.replace('[', ' [')
+        utterance = utterance.replace(']', '] ')
+        utterance = utterance.replace('“', ' “ ')
+        utterance = utterance.replace('”', ' ” ')
+        utterance = re.sub('[^\+],', ' , ', utterance)
+        utterance = re.sub('[^\[\./]\?', ' ? ', utterance)
+        # utterance = re.sub('[^\(\[\.\+]\.', ' . ', utterance)
+        utterance = remove_extra_spaces(utterance)
+        # print('step 2:', utterance)
 
-    utterance = utterance.replace('<', ' <')
-    utterance = utterance.replace('+ <', '+<')
-    utterance = utterance.replace('>', '> ')
-    utterance = utterance.replace('[', ' [')
-    utterance = utterance.replace(']', '] ')
-    utterance = utterance.replace('“', ' “ ')
-    utterance = utterance.replace('”', ' ” ')
-    utterance = re.sub('[^\+],', ' , ', utterance)
-    utterance = re.sub('[^\[\./]\?', ' ? ', utterance)
-    # utterance = re.sub('[^\(\[\.\+]\.', ' . ', utterance)
-    utterance = remove_extra_spaces(utterance)
-    # print('step 2:', utterance)
+        # Step 3:
+        # Handle [/], [//], [///] for repetitions/reformulation
+        #        [: xx] or [:: xx] for errors
+        #
+        # Discard "xx [/]", "<xx yy> [/]", "xx [//]", "<xx yy> [//]".
+        # For "zz [: xx]" or "<yy zz> [:: xx]", keep "xx" and discard the rest.
+        #
+        # Strategies:
+        # 1. Get all matching index pairs for angle brackets < and >.
+        # 2. Delete the unwanted material inside and including these brackets
+        #    plus their signaling annotations (= "[:", "[::", "[/]", "[//]").
+        # 3. Delete the unwanted words on the left of the signaling annotations.
 
-    # Step 3:
-    # Handle [/], [//], [///] for repetitions/reformulation
-    #        [: xx] or [:: xx] for errors
-    #
-    # Discard "xx [/]", "<xx yy> [/]", "xx [//]", "<xx yy> [//]".
-    # For "zz [: xx]" or "<yy zz> [:: xx]", keep "xx" and discard the rest.
-    #
-    # Strategies:
-    # 1. Get all matching index pairs for angle brackets < and >.
-    # 2. Delete the unwanted material inside and including these brackets plus
-    #    their signaling annotations (= "[:", "[::", "[/]", "[//]").
-    # 3. Delete the unwanted words on the left of the signaling annotations.
+        angle_brackets_l2r_pairs = dict() # left-to-right
+        for index_ in find_indices(utterance, '<'):
+            counter = 1
+            for i in range(index_ + 1, len(utterance)):
+                if utterance[i] == '<':
+                    counter += 1
+                elif utterance[i] == '>':
+                    counter -= 1
 
-    angle_brackets_l2r_pairs = dict() # left-to-right
-    for index_ in find_indices(utterance, '<'):
-        counter = 1
-        for i in range(index_ + 1, len(utterance)):
-            if utterance[i] == '<':
-                counter += 1
-            elif utterance[i] == '>':
-                counter -= 1
+                if counter == 0:
+                    angle_brackets_l2r_pairs[index_] = i
+                    break
+        angle_brackets_r2l_pairs = {v: k
+                                    for k, v in angle_brackets_l2r_pairs.items()}
 
-            if counter == 0:
-                angle_brackets_l2r_pairs[index_] = i
-                break
-    angle_brackets_r2l_pairs = {v: k
-                                for k, v in angle_brackets_l2r_pairs.items()}
+        index_pairs = list()  # characters bounded by index pairs to be removed
 
-    index_pairs = list()  # characters bounded by index pairs are to be removed
+        # remove ' [///]'
+        triple_slash_right_indices = find_indices(utterance, '> \[///\]')
+        index_pairs += [(begin + 1, begin + 6)
+                        for begin in triple_slash_right_indices]
 
-    triple_slash_right_indices = find_indices(utterance, '> \[///\]')
-    index_pairs += [(begin + 1, begin + 6)
-                    for begin in triple_slash_right_indices]  # remove ' [///]'
+        # remove ' [//]'
+        double_overlap_right_indices = find_indices(utterance, '> \[//\]')
+        index_pairs += [(begin + 1, begin + 5)
+                        for begin in double_overlap_right_indices]
 
-    double_overlap_right_indices = find_indices(utterance, '> \[//\]')
-    index_pairs += [(begin + 1, begin + 5)
-                    for begin in double_overlap_right_indices]  # remove ' [//]'
+        # remove ' [/]'
+        single_overlap_right_indices = find_indices(utterance, '> \[/\]')
+        index_pairs += [(begin + 1, begin + 4)
+                        for begin in single_overlap_right_indices]
 
-    single_overlap_right_indices = find_indices(utterance, '> \[/\]')
-    index_pairs += [(begin + 1, begin + 4)
-                    for begin in single_overlap_right_indices]  # remove ' [/]'
+        # remove ' [::'
+        double_error_right_indices = find_indices(utterance, '> \[::')
+        index_pairs += [(begin + 1, begin + 4)
+                        for begin in double_error_right_indices]
 
-    double_error_right_indices = find_indices(utterance, '> \[::')
-    index_pairs += [(begin + 1, begin + 4)
-                    for begin in double_error_right_indices]  # remove ' [::'
+        # remove ' [:'
+        single_error_right_indices = find_indices(utterance, '> \[: ')
+        index_pairs += [(begin + 1, begin + 3)
+                        for begin in single_error_right_indices]
 
-    single_error_right_indices = find_indices(utterance, '> \[: ')
-    index_pairs += [(begin + 1, begin + 3)
-                    for begin in single_error_right_indices]  # remove ' [:'
+        right_indices = double_overlap_right_indices + \
+                        single_overlap_right_indices + \
+                        double_error_right_indices + \
+                        single_error_right_indices + \
+                        triple_slash_right_indices
+        index_pairs = index_pairs + [(angle_brackets_r2l_pairs[right], right)
+                                      for right in sorted(right_indices)]
+        indices_to_ignore = set()
+        for left, right in index_pairs:
+            for i in range(left, right + 1):
+                indices_to_ignore.add(i)
 
-    right_indices = double_overlap_right_indices + \
-                    single_overlap_right_indices + \
-                    double_error_right_indices + \
-                    single_error_right_indices + \
-                    triple_slash_right_indices
-    index_pairs = index_pairs + [(angle_brackets_r2l_pairs[right], right)
-                                  for right in sorted(right_indices)]
-    indices_to_ignore = set()
-    for left, right in index_pairs:
-        for i in range(left, right + 1):
-            indices_to_ignore.add(i)
+        new_utterance = ''
+        for i in range(len(utterance)):
+            if i not in indices_to_ignore:
+                new_utterance += utterance[i]
+        utterance = new_utterance
 
-    new_utterance = ''
-    for i in range(len(utterance)):
-        if i not in indices_to_ignore:
-            new_utterance += utterance[i]
-    utterance = new_utterance
+        utterance = re.sub('\S+? \[/\]', '', utterance)
+        utterance = re.sub('\S+? \[//\]', '', utterance)
+        utterance = re.sub('\S+? \[///\]', '', utterance)
 
-    utterance = re.sub('\S+? \[/\]', '', utterance)
-    utterance = re.sub('\S+? \[//\]', '', utterance)
-    utterance = re.sub('\S+? \[///\]', '', utterance)
+        utterance = re.sub('\S+? \[::', '', utterance)
+        utterance = re.sub('\S+? \[:', '', utterance)
 
-    utterance = re.sub('\S+? \[::', '', utterance)
-    utterance = re.sub('\S+? \[:', '', utterance)
+        utterance = remove_extra_spaces(utterance)
+        # print('step 3:', utterance)
 
-    utterance = remove_extra_spaces(utterance)
-    # print('step 3:', utterance)
+        # Step 4: Split utterance by spaces and determine whether to keep items.
 
-    # Step 4: Split utterance by spaces and determine whether to keep items.
+        escape_prefixes = {'[?', '[/', '[<', '[>', '[:', '[!', '[*',
+                           '+"', '+,', '&', '<&'}
+        escape_words = {'0', '++', '+<',
+                        '(.)', '(..)', '(...)',
+                        'xxx', 'www', 'yyy'}
+        keep_prefixes = {'+"/', '+,/', '+".'}
 
-    escape_prefixes = {'[?', '[/', '[<', '[>', '[:', '[!', '[*',
-                       '+"', '+,', '&', '<&'}
-    escape_words = {'0', '++', '+<',
-                    '(.)', '(..)', '(...)',
-                    'xxx', 'www', 'yyy'}
-    keep_prefixes = {'+"/', '+,/', '+".'}
+        words = utterance.split()
+        new_words = list()
 
-    words = utterance.split()
-    new_words = list()
+        for word in words:
+            word = re.sub('\A<', '', word)   # remove beginning <
+            word = re.sub('>\Z', '', word)   # remove final >
+            word = re.sub('\]\Z', '', word)  # remove final ]
 
-    for word in words:
-        word = re.sub('\A<', '', word)   # remove beginning <
-        word = re.sub('>\Z', '', word)   # remove final >
-        word = re.sub('\]\Z', '', word)  # remove final ]
+            not_an_escape_word = word not in escape_words
+            no_escape_prefix = not startswithoneof(word, escape_prefixes)
+            has_keep_prefix = startswithoneof(word, keep_prefixes)
 
-        not_an_escape_word = word not in escape_words
-        no_escape_prefix = not startswithoneof(word, escape_prefixes)
-        has_keep_prefix = startswithoneof(word, keep_prefixes)
+            if (not_an_escape_word and no_escape_prefix) or has_keep_prefix:
+                new_words.append(word)
 
-        if (not_an_escape_word and no_escape_prefix) or has_keep_prefix:
-            new_words.append(word)
+        # print('step 4:', remove_extra_spaces(' '.join(new_words)))
 
-    # print('step 4:', remove_extra_spaces(' '.join(new_words)))
+        return remove_extra_spaces(' '.join(new_words))
 
-    return remove_extra_spaces(' '.join(new_words))
+    @staticmethod
+    def _get_participant_code(tier_marker_seq):
+        """
+        Return the participant code from a tier marker set.
 
+        :param tier_marker_seq: A sequence of tier markers like
+            ``{'CHI', '%mor', '%gra'}``
 
-def get_participant_code(tier_marker_seq):
-    """
-    Return the participant code from a tier marker set.
+        :return: A participant code, e.g., ``'CHI'``
 
-    :param tier_marker_seq: A sequence of tier markers like
-        ``{'CHI', '%mor', '%gra'}``
-
-    :return: A participant code, e.g., ``'CHI'``
-
-    :rtype: str, or None if no participant code is found
-    """
-    for tier_marker in tier_marker_seq:
-        if not tier_marker.startswith('%'):
-            return tier_marker
-    return None
+        :rtype: str, or None if no participant code is found
+        """
+        for tier_marker in tier_marker_seq:
+            if not tier_marker.startswith('%'):
+                return tier_marker
+        return None

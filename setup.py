c88bf9dee4a48aea0b35fa8a93ef923c3363b7b3
jacksonllee <jacksonlunlee@gmail.com>
2017-05-07 12:53:06 -0500

    MAINT Update setup.py

--- setup.py
+++ setup.py
@@ -1,55 +1,69 @@
+# -*- coding: utf-8 -*-
+
 from os import path
-import sys
 from setuptools import (setup, find_packages)
 
-py_version = sys.version_info[:2]
-if py_version < (3, 4):
-    sys.exit('Error: PyLangAcq requires Python 3.4 or above.\n'
-             'You are using Python {}.{}.'.format(*py_version))
 
-version_path = path.join(path.dirname(__file__), 'pylangacq', 'VERSION')
-with open(version_path) as f:
+THIS_DIR = path.dirname(__file__)
+
+with open(path.join(THIS_DIR, 'pylangacq', 'VERSION')) as f:
     package_version = f.read().strip()
 
-setup(name="pylangacq",
-    version=package_version,
-    description="PyLangAcq",
-    long_description="PyLangAcq: Language Acquisition Research in Python",
-    url="http://pylangacq.org/",
-    author="Jackson Lee",
-    author_email="jsllee.phon@gmail.com",
-    license="Apache License, Version 2.0",
-    packages=find_packages(),
-    keywords=['computational linguistics', 'natural language processing',
-              'NLP', 'linguistics', 'corpora', 'speech',
-              'language', 'CHILDES', 'CHAT', 'transcription',
-              'acquisition', 'development', 'learning'],
+with open(path.join(THIS_DIR, 'README.rst')) as f:
+    long_description = f.read()
 
-    install_requires=['networkx'],
+with open(path.join(THIS_DIR, 'requirements.txt')) as f:
+    requirements = [x.strip() for x in f.readlines()]
 
-    zip_safe=False,
 
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Text Processing',
-        'Topic :: Text Processing :: Filters',
-        'Topic :: Text Processing :: General',
-        'Topic :: Text Processing :: Indexing',
-        'Topic :: Text Processing :: Linguistic'
-    ],
+def main():
+    setup(
+        name="pylangacq",
+        version=package_version,
+        description="PyLangAcq: Language Acquisition Research in Python",
+        long_description=long_description,
+        url="http://pylangacq.org/",
+        author="Jackson Lee",
+        author_email="jacksonlunlee@gmail.com",
+        license="MIT License",
+        packages=find_packages(),
+        keywords=['computational linguistics', 'natural language processing',
+                  'NLP', 'linguistics', 'corpora', 'speech',
+                  'language', 'CHILDES', 'CHAT', 'transcription',
+                  'acquisition', 'development', 'learning'],
 
-    package_data = {'pylangacq': ['VERSION']}
-)
+        package_data={'pylangacq': ['VERSION']},
+
+        install_requires=requirements,
+
+        zip_safe=False,
+
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'Intended Audience :: Information Technology',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+            'Topic :: Scientific/Engineering :: Information Analysis',
+            'Topic :: Text Processing',
+            'Topic :: Text Processing :: Filters',
+            'Topic :: Text Processing :: General',
+            'Topic :: Text Processing :: Indexing',
+            'Topic :: Text Processing :: Linguistic'
+        ],
+    )
+
+
+if __name__ == "__main__":
+    main()

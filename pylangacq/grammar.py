37451353cf22f14d0e9e61a74ae756f8e92fbf09
jacksonllee <jacksonlunlee@gmail.com>
2017-05-07 12:38:12 -0500

    TST Add test_dependency.py

--- pylangacq/grammar.py
+++ pylangacq/grammar.py
@@ -62,26 +62,6 @@ class DependencyGraph(nx.DiGraph):
         """
         Return the dependency graph as LaTeX tikz-dependency code.
 
-        >>> from pylangacq.grammar import (DependencyGraph, chat_graph_data)
-        >>> graph = DependencyGraph(chat_graph_data)
-        >>> print(graph.to_tikz())
-        \begin{dependency}[theme = simple]
-            \begin{deptext}[column sep=1em]
-                but \& I \& thought \& you \& wanted \& me \& to \& turn \& it \& . \\
-            \end{deptext}
-            \deproot{3}{ROOT}
-            \depedge{1}{3}{LINK}
-            \depedge{2}{3}{SUBJ}
-            \depedge{3}{0}{ROOT}
-            \depedge{4}{3}{OBJ}
-            \depedge{5}{3}{JCT}
-            \depedge{6}{5}{POBJ}
-            \depedge{7}{8}{INF}
-            \depedge{8}{3}{XCOMP}
-            \depedge{9}{8}{OBJ}
-            \depedge{10}{3}{PUNCT}
-        \end{dependency}
-
         :return: The LaTeX tikz-dependency code for drawing the graph
 
         :rtype: str
@@ -129,20 +109,6 @@ class DependencyGraph(nx.DiGraph):
         """
         Return the dependency graph in the CoNLL format.
 
-        >>> from pylangacq.grammar import (DependencyGraph, chat_graph_data)
-        >>> graph = DependencyGraph(chat_graph_data)
-        >>> print(graph.to_conll())
-        but CONJ 3 LINK
-        I PRO:SUB 3 SUBJ
-        thought V 0 ROOT
-        you PRO 3 OBJ
-        wanted V 3 JCT
-        me PRO:OBJ 5 POBJ
-        to INF 8 INF
-        turn V 3 XCOMP
-        it PRO 8 OBJ
-        . . 3 PUNCT
-
         :return: The CoNLL format of the dependency graph
 
         :rtype: str
@@ -158,20 +124,3 @@ class DependencyGraph(nx.DiGraph):
             collector.append('{} {} {} {}'.format(word, pos, head, rel))
 
         return '\n'.join(collector)
-
-
-chat_graph_data = [('but', 'CONJ', 'but', (1, 3, 'LINK')),
-                   ('I', 'PRO:SUB', 'I', (2, 3, 'SUBJ')),
-                   ('thought', 'V', 'think&PAST', (3, 0, 'ROOT')),
-                   ('you', 'PRO', 'you', (4, 3, 'OBJ')),
-                   ('wanted', 'V', 'want-PAST', (5, 3, 'JCT')),
-                   ('me', 'PRO:OBJ', 'me', (6, 5, 'POBJ')),
-                   ('to', 'INF', 'to', (7, 8, 'INF')),
-                   ('turn', 'V', 'turn', (8, 3, 'XCOMP')),
-                   ('it', 'PRO', 'it', (9, 8, 'OBJ')),
-                   ('.', '.', '', (10, 3, 'PUNCT')),
-                   ]
-
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod(verbose=True)

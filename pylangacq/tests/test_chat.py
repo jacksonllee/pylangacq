4ec1f0a140f92cd945cd01c7619f0bdafc604240
jacksonllee <jacksonlunlee@gmail.com>
2017-10-30 13:17:50 -0500

    TST Test number_of_files method

--- pylangacq/tests/test_chat.py
+++ pylangacq/tests/test_chat.py
@@ -1,35 +1,47 @@
+import os
 import zipfile
 
 import pytest
 import requests
+
+from pylangacq import read_chat
 
 
 BROWN_URL = 'https://childes.talkbank.org/data/Eng-NA/Brown.zip'
 BROWN_ZIP_PATH = 'brown.zip'
 
 
-def download_brown_zip(zip_path):
+def download_file(file_path):
     try:
-        with open(zip_path, 'wb') as brown_zip_file:
+        with open(file_path, 'wb') as f:
             with requests.get(BROWN_URL) as r:
-                brown_zip_file.write(r.content)
+                f.write(r.content)
     except Exception as e:  # pragma: no cover
-        msg = ('Error in downloading Brown zip: '
+        msg = ('Error in downloading {}: '
                'network problems or invalid URL for Brown zip? '
                'If URL needs updating, tutorial.rst in docs '
-               'has to be updated as well.')
+               'has to be updated as well.'.format(BROWN_URL))
         try:
             raise e
         finally:
             pytest.exit(msg)
 
 
-def test_brown_zip_is_available():
-    """Verify Brown zip is available for downeload and can be unzipped."""
-    download_brown_zip(BROWN_ZIP_PATH)
-    with zipfile.ZipFile(BROWN_ZIP_PATH) as brown_zip:
-        brown_zip.extractall()
+@pytest.fixture(scope='module')
+def brown_zip_path():
+    """Make sure Brown zip is available for download and can be unzipped."""
+    zip_path = BROWN_ZIP_PATH
+    download_file(zip_path)
+    return zip_path
 
-# NOTE: No new tests above this line
-# pytest runs tests by the order they are defined, and so
-# test_brown_zip_is_available() must run first to download the Brown zip file.
+
+@pytest.fixture(scope='module')
+def eve(brown_zip_path):
+    with zipfile.ZipFile(brown_zip_path) as zip_file:
+        zip_file.extractall()
+    eve_data_path = os.path.join('Brown', 'Eve', '*.cha')
+    return read_chat(eve_data_path, encoding='utf-8')
+
+
+def test_number_of_files(eve):
+    assert len(eve) == eve.number_of_files() == 20

dc566381f7176fde60936aec50c08d6df83803ab
jacksonllee <jacksonlunlee@gmail.com>
2017-10-30 00:08:41 -0500

    TST Keep brown zip around for other tests

--- pylangacq/tests/test_chat.py
+++ pylangacq/tests/test_chat.py
@@ -1,12 +1,12 @@
 from __future__ import print_function
 
 import zipfile
-import tempfile
 
 import requests
 
 
 BROWN_URL = 'https://childes.talkbank.org/data/Eng-NA/Brown.zip'
+BROWN_ZIP_PATH = 'brown.zip'
 
 
 def download_brown_zip(zip_path):
@@ -23,10 +23,11 @@ def download_brown_zip(zip_path):
 
 
 def test_brown_zip_is_available():
-    """Verify Brown zip is available for download and can be unzipped."""
-    with tempfile.NamedTemporaryFile() as temp_zip_file:
-        zip_path = temp_zip_file.name
+    """Verify Brown zip is available for downeload and can be unzipped."""
+    download_brown_zip(BROWN_ZIP_PATH)
+    with zipfile.ZipFile(BROWN_ZIP_PATH) as brown_zip:
+        brown_zip.extractall()
 
-        download_brown_zip(zip_path)
-        with zipfile.ZipFile(zip_path) as brown_zip:
-            brown_zip.extractall()
+# NOTE: No new tests above this line
+# pytest runs tests by the order they are defined, and so
+# test_brown_zip_is_available() must run first to download the Brown zip file.

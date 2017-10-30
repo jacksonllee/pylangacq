64fab2943dc9a4910eba3bac1cca0ee65480b7ba
jacksonllee <jacksonlunlee@gmail.com>
2017-10-29 22:38:37 -0500

    TST Check that URL for Brown zip is invalid

---
+++ pylangacq/tests/test_chat.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import zipfile
+import tempfile
+
+import requests
+
+
+BROWN_URL = 'https://childes.talkbank.org/data/Eng-NA/Brown.zip'
+
+
+def download_brown_zip(zip_path):
+    try:
+        with open(zip_path, 'wb') as brown_zip_file:
+            with requests.get(BROWN_URL) as r:
+                brown_zip_file.write(r.content)
+    except:  # pragma: no cover  # noqa
+        print('Error in downloading Brown zip: '
+              'network problems or invalid URL for Brown zip?\n'
+              'If URL needs updating, tutorial.rst in docs has to be updated '
+              'as well\n.')
+        raise
+
+
+def extract_brown_zip(zip_path):
+    with zipfile.ZipFile(zip_path) as brown_zip:
+        brown_zip.extractall()
+
+
+def test_brown_zip_is_available():
+    with tempfile.NamedTemporaryFile() as temp_zip_file:
+        zip_path = temp_zip_file.name
+
+        download_brown_zip(zip_path)
+        extract_brown_zip(zip_path)

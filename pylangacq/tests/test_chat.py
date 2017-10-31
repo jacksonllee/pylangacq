5363b00d82d0e02f2bb3590bdc4b6a92375f3538
jacksonllee <jacksonlunlee@gmail.com>
2017-10-31 12:53:09 -0500

    TST Cover update in Reader

--- pylangacq/tests/test_chat.py
+++ pylangacq/tests/test_chat.py
@@ -11,9 +11,12 @@ BROWN_URL = 'https://childes.talkbank.org/data/Eng-NA/Brown.zip'
 BROWN_ZIP_PATH = 'brown.zip'
 
 
-def download_file(file_path):
+def test_download_and_extract_brown_zip_file():
+    """pytest runs tests in the same order they are defined in the test
+    module, and so this test for downloading and unzipping the Brown zip
+    data file runs first. If download fails, abort all tests."""
     try:
-        with open(file_path, 'wb') as f:
+        with open(BROWN_ZIP_PATH, 'wb') as f:
             with requests.get(BROWN_URL) as r:
                 f.write(r.content)
     except Exception as e:  # pragma: no cover
@@ -25,21 +28,15 @@ def download_file(file_path):
             raise e
         finally:
             pytest.exit(msg)
+    else:
+        # If download succeeds, unzip the Brown zip file.
+        with zipfile.ZipFile(BROWN_ZIP_PATH) as zip_file:
+            zip_file.extractall()
 
 
-@pytest.fixture(scope='module')
-def brown_zip_path():
-    """Make sure Brown zip is available for download and can be unzipped."""
-    zip_path = BROWN_ZIP_PATH
-    download_file(zip_path)
-    return zip_path
-
-
-@pytest.fixture(scope='module')
-def eve(brown_zip_path):
-    with zipfile.ZipFile(brown_zip_path) as zip_file:
-        zip_file.extractall()
-    eve_data_path = os.path.join('Brown', 'Eve', '*.cha')
+@pytest.fixture
+def eve():
+    eve_data_path = os.path.join('Brown', 'Eve', '010600a.cha')
     return read_chat(eve_data_path, encoding='utf-8')
 
 
@@ -49,4 +46,15 @@ def test_read_chat_wrong_filename_type():
 
 
 def test_number_of_files(eve):
-    assert len(eve) == eve.number_of_files() == 20
+    assert len(eve) == eve.number_of_files() == 1
+
+
+def test_update(eve):
+    new_eve = read_chat(os.path.join('Brown', 'Eve', '010600b.cha'))
+    eve.update(new_eve)
+    assert len(eve) == eve.number_of_files() == 2
+
+
+def test_update_wrong_reader_type(eve):
+    with pytest.raises(ValueError):
+        eve.update(42)

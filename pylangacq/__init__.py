5508e6a50eb9ac413edce3e61cdf74da90505751
jacksonllee <jacksonlunlee@gmail.com>
2020-06-30 19:02:22 -0500

    MAINT updates to packaging, builds, dev env

--- pylangacq/__init__.py
+++ pylangacq/__init__.py
@@ -1,5 +1,8 @@
-from pylangacq.chat import read_chat, Reader
-from pylangacq._version import __version__
+import pkg_resources
 
+from pylangacq.chat import read_chat, Reader
+
+
+__version__ = pkg_resources.get_distribution("pylangacq").version
 
 __all__ = ["__version__", "read_chat", "Reader"]
